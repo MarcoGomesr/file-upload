ea135a290fea0fd4c2a1d5147164cbaecb28281e
Marco Gomes <marcogomesr@gmail.com>
2024-11-27 10:19:05 +0100

    add delete button

--- app/components/ui/DeleteButton.tsx
+++ app/components/ui/DeleteButton.tsx
@@ -5,14 +5,13 @@ import { Button } from "./Button"
 export function DeleteButton() {
   const { pending } = useFormStatus()
 
-  const renderIcon = () => {
-    if (pending) <Loader2 className="size-4 animate-spin" />
-    return <XIcon className="size-4" />
-  }
-
   return (
     <Button size="icon" variant="destructive" disabled={pending}>
-      {renderIcon()}
+      {pending ? (
+        <Loader2 className="size-4 animate-spin" />
+      ) : (
+        <XIcon className="size-4" />
+      )}
     </Button>
   )
 }
